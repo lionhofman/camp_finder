b8312dfa86a10555ec7c14c8164b2b9d8c446e33
Lion Hofman <lionhofman@gmail.com>
2023-10-24 07:52:47 -0300

    ✨ feat: added address, map generated on google maps and distance from the campsite to the user, all linked to google maps

--- lib/app/ui/modules/detail/controller/item_detail_controller.dart
+++ lib/app/ui/modules/detail/controller/item_detail_controller.dart
@@ -1,14 +1,57 @@
 import 'package:camp_finder/app/domain/entities/camping.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ItemDetailController extends GetxController {
   Rxn<Camping> _campingDetail = Rxn<Camping>();
-
+  var _distanceBetween = "".obs;
+  Rxn<CameraPosition> _kGooglePlex = Rxn<CameraPosition>();
+  String? get distanceBetween => _distanceBetween.value;
   Rxn<Camping> get campingDetail => _campingDetail;
+  Rxn<CameraPosition> get kGooglePlex => _kGooglePlex;
+  validateLaunchURL(String? address) async {
+    if (address != null) {
+      String encodedAddress = Uri.encodeComponent(address);
+      String url =
+          'https://www.google.com/maps/search/?api=1&query=$encodedAddress';
+
+      Uri myUri = Uri.parse(url);
+      await launchUrl(
+        myUri,
+        mode: LaunchMode.externalApplication,
+      );
+    }
+  }
+
   @override
   void onInit() {
     super.onInit();
     _campingDetail.value = Get.arguments;
+    calculateDistance();
+    getCameraPosition();
+  }
+
+  void getCameraPosition() {
+    GeoPoint point = _campingDetail.value!.position['geopoint'];
+    _kGooglePlex.value = CameraPosition(
+      target: LatLng(point.latitude, point.longitude),
+      zoom: 14.0,
+    );
+  }
+
+  Future<void> calculateDistance() async {
+    GeoPoint? point = _campingDetail.value!.position['geopoint'];
+    final currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    if (point != null) {
+      final distance = Geolocator.distanceBetween(currentPosition.latitude,
+          currentPosition.longitude, point.latitude, point.longitude);
+      _distanceBetween.value =
+          "${(distance / 1000).toStringAsFixed(2).replaceAll('.', ',')} km";
+    }
   }
 }
--- lib/app/ui/modules/detail/tabs/about_us.dart
+++ lib/app/ui/modules/detail/tabs/about_us.dart
@@ -1,19 +1,21 @@
+import 'dart:async';
+
 import 'package:camp_finder/app/core/constants/colors.dart';
 
 import 'package:camp_finder/app/domain/entities/camping.dart';
+import 'package:camp_finder/app/ui/modules/detail/controller/item_detail_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:nb_utils/nb_utils.dart';
 
-class AboutUs extends StatefulWidget {
+class AboutUs extends GetView<ItemDetailController> {
   final Camping? campDetails;
-  const AboutUs({Key? key, this.campDetails}) : super(key: key);
+  final Completer<GoogleMapController> googleMapsController =
+      Completer<GoogleMapController>();
+  AboutUs({Key? key, this.campDetails}) : super(key: key);
 
-  @override
-  State<AboutUs> createState() => _AboutUsState();
-}
-
-class _AboutUsState extends State<AboutUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,45 +31,68 @@ class _AboutUsState extends State<AboutUs> {
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
               const SizedBox(height: 10),
               Text(
-                widget.campDetails != null ? widget.campDetails!.about : "",
+                campDetails != null ? campDetails!.about : "",
                 style: TextStyle(fontSize: 16, color: Colors.grey[600]),
               ),
               const SizedBox(height: 20),
               const Text('Endereço',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               const SizedBox(height: 10),
-              Text(
-                widget.campDetails != null ? widget.campDetails!.address : "",
-                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-              ),
-              const SizedBox(height: 10),
-              TextButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.directions, color: appColorPrimary),
-                label: const Text(
-                  'Get Directions - 0.2 KM',
-                  style: TextStyle(color: appColorPrimary),
+              GestureDetector(
+                onTap: () {
+                  controller.validateLaunchURL(campDetails!.address);
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.home, color: appColorPrimary),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Text(
+                            campDetails!.address,
+                            style: const TextStyle(
+                                color: appColorPrimary,
+                                decoration: TextDecoration.underline),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Obx(
+                      () => Row(
+                        children: [
+                          const Icon(Icons.location_on, color: appColorPrimary),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              controller.distanceBetween != null
+                                  ? "Está à aproximadamente ${controller.distanceBetween}"
+                                  : "",
+                              style: const TextStyle(color: appColorPrimary),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
               const SizedBox(height: 10),
-              Container(
-                height: 100,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: const AssetImage('assets/images/ls_Map.jpg'),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                        Colors.black.withOpacity(0.4), BlendMode.darken),
-                  ),
-                ),
-                alignment: Alignment.center,
-                child: const Text(
-                  'Show Map',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+              const Text('Veja no mapa',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 120.0,
+                child: GoogleMap(
+                  onTap: (argument) {
+                    controller.validateLaunchURL(campDetails!.address);
+                  },
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: controller.kGooglePlex.value!,
+                  onMapCreated: (GoogleMapController controller) {
+                    googleMapsController.complete(controller);
+                  },
                 ),
               ),
               const SizedBox(height: 20),
@@ -75,20 +100,20 @@ class _AboutUsState extends State<AboutUs> {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               const SizedBox(height: 10),
               ListView.builder(
-                itemCount: widget.campDetails!.openHours != null
-                    ? widget.campDetails!.openHours!.length
+                itemCount: campDetails!.openHours != null
+                    ? campDetails!.openHours!.length
                     : 0,
                 shrinkWrap: true,
                 itemBuilder: (BuildContext context, int index) {
                   return Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text("${widget.campDetails!.openHours![index].day}:",
+                      Text("${campDetails!.openHours![index].day}:",
                           style: const TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold)),
                       Padding(
                         padding: const EdgeInsets.only(left: 16),
-                        child: Text(widget.campDetails!.openHours![index].hours,
+                        child: Text(campDetails!.openHours![index].hours,
                             style:
                                 const TextStyle(fontWeight: FontWeight.w100)),
                       ),
